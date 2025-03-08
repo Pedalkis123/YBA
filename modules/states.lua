fdef35cbab8dbe52d7ea8de92c26b86bf3ab3f67
Pedalkis123 <himmlerhitler2@gmail.com>
2025-03-08 20:31:51 +0200

    Create states.lua

---
+++ modules/states.lua
@@ -0,0 +1,16 @@
+-- States module for toggle states
+
+local States = {
+    Speed = false,
+    Jump = false,
+    AntiTimestop = false,
+    AntiReset = false,
+    AntiTeleport = false,
+    ItemFarm = false,
+    QuestFarm = false,
+    ItemNotify = false,
+    StandFarm = false,
+    ShinySniping = false
+}
+
+return States
